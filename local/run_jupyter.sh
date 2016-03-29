3252fb451a1dba0e9008e556530248ed5caf42e4
MuXian Wu <muxian.wu@gogame.net>
2016-03-29 11:01:48 +0800

    fixed bugs

--- local/run_jupyter.sh
+++ local/run_jupyter.sh
@@ -1,6 +1,6 @@
 DOCKER_MACHINE=${DOCKER_MACHINE:-mydockermachine}
 HOST_IP=$(docker-machine ip $DOCKER_MACHINE)
-SPARK_IMAGE=${SPARK_IMAGE:-"wumuxian/spark-mesos:2.6.0-0.27.0"}
+SPARK_IMAGE=${SPARK_IMAGE:-"wumuxian/spark-mesos:1.6.0-0.27.0"}
 SPARK_MASTER=${SPARK_MASTER:-"mesos://zk://${HOST_IP}:2181/mesos"}
 echo Setting spark image:
 echo $SPARK_IMAGE
